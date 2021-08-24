065d9efc610db89f3d93c49aaa5254638b48e9b7
Minhaz78 <minhazic78@gmail.com>
2021-08-24 08:48:13 +0600

    ==vs ===

---
+++ double vs triple-equal.js
@@ -0,0 +1,46 @@
+//==(it check only value )
+//===(it check value and type)
+
+//double equal with false value 
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+
+//triple equal
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+double equal with true value 
+const first = 1;
+const second = true;
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+//triple equal
+const first = 1;
+const second = true;
+if(first === second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
