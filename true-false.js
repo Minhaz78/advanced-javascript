0048c38d9740e15a060d177dd88c24d02c5c59be
Minhaz78 <minhazic78@gmail.com>
2021-08-24 08:34:02 +0600

    update

--- true-false.js
+++ true-false.js
@@ -1,17 +1,17 @@
-//const name = "0";
+const name = "0";
 
 
-// when condition is false
-// 1)if you not set variable value
-// 2)name=null
-//3)name=NaN
-//4)name=false
+when condition is false
+1)if you not set variable value
+2)name=null
+3)name=NaN
+4)name=false
 
-//when condition is true
-//1)'0' and ' '(space inside)
-//2)name=[];
-//3)name={};
-//4)name ='false'
+when condition is true
+1)'0' and ' '(space inside)
+2)name=[];
+3)name={};
+4)name ='false'
 
 let name=0;
 if(name || name==0){
