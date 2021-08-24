96207e3aa235db8b931ac68321e21fa641651be1
Minhaz78 <minhazic78@gmail.com>
2021-08-24 08:32:56 +0600

    null vs undefined

---
+++ null vs undefined.js
@@ -0,0 +1,26 @@
+how to find undefined value :-
+
+1)let khan;
+console.log(khan);
+
+//2)function add(num1,num2){
+    console.log(num1+num2);
+}
+const result = add(12,13);
+console.log(result);
+
+3)function add(num1,num2){
+         console.log(num1,num2);
+         return
+     }
+     const result = add(13);
+     console.log(result);
+    
+4)const premik = {name:"smart dude",phone:2345248723};
+console.log(premik.gf);
+
+5)let fun= undefined;
+console.log(fun);
+
+
+null----its assigned by developers
